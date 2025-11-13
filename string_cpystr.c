6c16dad8e29c94d188515c040c93ac08302b6824
yashvi_janjrukia <yashvijanjrukia5@gmail.com>
2025-11-13 20:01:00 +0530

    Update string_cpystr.c

---
+++ string_cpystr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s1[100], s2[100];
+    printf("Enter a string: ");
+    gets(s1);
+    strcpy(s2, s1);
+    printf("Copied string: %s", s2);
+    return 0;
+}
